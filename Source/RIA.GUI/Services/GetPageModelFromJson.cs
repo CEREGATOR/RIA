e312b1355fb561a5bfcee7620f54aa8f96b394d5
CEREGATOR <s.belogrydov@ya.ru>
2021-02-11 19:16:37 +0300

    Бизнес-логика перенесена в слой служб. Получение модели страницы перенесён в отдельный класс. Исправена ошибка при нажатии на пустое поле в ListJsonFiles

---
+++ Source/RIA.GUI/Services/GetPageModelFromJson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using RIA.Grabber;
+using RIA.Grabber.Model;
+using RIA.Grabber.Services;
+
+namespace RIA.GUI.Services
+{
+    public class GetPageModelFromJson
+    {
+        public PageModel PageModelJson(string filename, string path)
+        {
+            var jsonString = File.ReadAllText(Path.Combine(path, filename));
+            var pageModel = JsonSerializer.Deserialize<PageModel>(jsonString);
+
+            return pageModel;
+        }
+    }
+}
