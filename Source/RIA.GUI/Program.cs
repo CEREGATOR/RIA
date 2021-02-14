bb4b33a28be5cbcf112dde6606461e967b455bd7
CEREGATOR <s.belogrydov@ya.ru>
2021-02-14 15:10:40 +0300

    Добавлено инициализация CompositionRoot в Program.cs и вызов формы в Application.Run

--- Source/RIA.GUI/Program.cs
+++ Source/RIA.GUI/Program.cs
@@ -14,7 +14,9 @@ namespace RIA.GUI
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            var compositionRoot = new CompositionRoot();
+            compositionRoot.Initialize();
+            Application.Run(compositionRoot.MainForm);
         }
     }
 }
